96e24213839659329f6652a1dc6b439c0bfdb4e1
EditaMatevosyan <125731320+EditaMatevosyan@users.noreply.github.com>
2024-03-31 10:54:21 +0400

    Add files via upload

---
+++ selectionSort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr1[] = {12, 54, 23, 87, 38, 25, 15, -43};
+    int n = 8;
+    
+    for (int i = 0; i < n; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < n; j++)
+        if (arr1[j] > arr1[min]) 
+            min = j;
+        
+        if (min != i)
+        {
+        int temp = arr1[i];
+        arr1[i] = arr1[min];
+        arr1[min] = temp;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+        printf("%d\n", arr1[i]);
+    
+    return 0;
+}
